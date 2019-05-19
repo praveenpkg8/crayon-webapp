c49844b260629cca51039928338ca965fcb9e20a
praveen <praveenpkg8@gmail.com>
2019-05-20 00:00:04 +0530

    developed feeds dynamicaly for pic and description

--- src/app/newsfeed/feed/feed.component.ts
+++ src/app/newsfeed/feed/feed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL } from '../../httpLink';
+
 
 @Component({
   selector: 'app-feed',
@@ -7,13 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient){}
+  feeds: any = null;
   ngOnInit() {
+    this.http.get(URL + 'api/newsfeed')
+    .subscribe( res => {
+      this.feeds = res
+      console.log(this.feeds)
+    })
   }
+  url : string = "assets/uploads/"
   description: string = "With supporting text below as a natural lead-in to additional content.";
   profileName: string = "Jon Snow"
 
-  
+
+
+
 
 }
